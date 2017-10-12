e91c3899488ae33ba5f2573aebe7c08eb3cbf6eb
David Streeter <dstreet26@gmail.com>
2017-10-12 17:29:01 -0400

    adding the rest of the sketching with tachyons-safe classes

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -76,6 +76,7 @@ view model =
     div [ classes [ fl, w_100 ], class "bg-typeform-blue montserrat color-1" ]
         [ viewTopSection model
         , viewFirstQuestion model
+        , viewSecondQuestion model
         ]
 
 
@@ -124,34 +125,122 @@ viewTopSection model =
 
 
 viewFirstQuestion model =
-    div [ class "mt6 mh7 f3    vh-100" ]
-        [ span [ class " pr2" ]
-            [ span [ class "pr1 color-5" ]
+    div
+        [ classes
+            [ Tachyons.Classes.mt6
+            , Tachyons.Classes.mh7
+            , Tachyons.Classes.f3
+            , Tachyons.Classes.vh_100
+            ]
+        ]
+        [ span [ classes [ Tachyons.Classes.pr2 ] ]
+            [ span [ classes [ Tachyons.Classes.pr1 ], class "color-5" ]
                 [ text "1" ]
-            , span [ class " color-5 fa fa-arrow-right" ]
+            , span [ class "color-5 fa fa-arrow-right" ]
                 []
             ]
-        , span [ class "asdf" ]
+        , span []
             [ Html.b []
                 [ text "Hello" ]
             , text ". What's your name?*"
             ]
-        , div [ class "input--hoshi ml3 " ]
-            [ input [ class " input__field--hoshi", id "input-4", type_ "text" ]
+        , div [ classes [ Tachyons.Classes.ml3 ], class "input--hoshi" ]
+            [ input [ class "input__field--hoshi", id "input-4", type_ "text" ]
                 []
-            , label [ class " input__label--hoshi hoshi-color-4", for "input-4" ]
+            , label [ class "input__label--hoshi hoshi-color-4", for "input-4" ]
                 []
             ]
-        , div [ class "asdf pt2 ml3" ]
-            [ button [ class "button-reset  b bg-typeform-blue2 br2 pv2 ph3 color-3 bn typeform-button-hover pointer shadow-5" ]
-                [ span [ class "asdf" ]
+        , div
+            [ classes
+                [ Tachyons.Classes.pt2
+                , Tachyons.Classes.ml3
+                ]
+            ]
+            [ button
+                [ classes
+                    [ Tachyons.Classes.button_reset
+                    , Tachyons.Classes.b
+                    , Tachyons.Classes.br2
+                    , Tachyons.Classes.pv2
+                    , Tachyons.Classes.ph3
+                    , Tachyons.Classes.bn
+                    , Tachyons.Classes.pointer
+                    , Tachyons.Classes.shadow_5
+                    ]
+                , class "bg-typeform-blue2 color-3 typeform-button-hover"
+                ]
+                [ span []
                     [ text "OK" ]
                 , span [ class "fa fa-check" ]
                     []
                 ]
-            , span [ class "asdf f6 color-5" ]
+            , span [ classes [ Tachyons.Classes.f_6 ], class "color-5" ]
                 [ text "press ENTER" ]
             ]
         , Html.br []
             []
+        ]
+
+
+viewSecondQuestion model =
+    div []
+        [ div
+            [ classes
+                [ Tachyons.Classes.mt6
+                , Tachyons.Classes.mh7
+                , Tachyons.Classes.f3
+                , Tachyons.Classes.vh_100
+                ]
+            ]
+            [ span [ classes [ Tachyons.Classes.pr2 ] ]
+                [ span [ classes [ Tachyons.Classes.pr1 ], class "color-5" ]
+                    [ text "2" ]
+                , span [ class " color-5 fa fa-arrow-right" ]
+                    []
+                ]
+            , span []
+                [ text "Hi, asdf. What's your "
+                , Html.b []
+                    [ text "gender" ]
+                , text "?"
+                ]
+            , ul
+                [ classes
+                    [ Tachyons.Classes.list
+                    , Tachyons.Classes.mw5
+                    ]
+                , class "color-5"
+                ]
+                [ liElement "A" "hey"
+                , liElement "B" "hey"
+                , liElement "X" "hardcoded!"
+                ]
+            ]
+        ]
+
+
+liElement letter body =
+    li
+        [ classes
+            [ Tachyons.Classes.ba
+            , Tachyons.Classes.pa3
+            , Tachyons.Classes.br2
+            , Tachyons.Classes.b__black_40
+            , Tachyons.Classes.mv3
+            , Tachyons.Classes.pointer
+            ]
+        , class "bg-color-7 typeform-select-hover"
+        ]
+        [ span
+            [ classes
+                [ Tachyons.Classes.ba
+                , Tachyons.Classes.ph2
+                , Tachyons.Classes.pv1
+                , Tachyons.Classes.mr2
+                ]
+            , class "bg-color-8"
+            ]
+            [ text letter ]
+        , span []
+            [ text body ]
         ]
