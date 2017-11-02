6f3b26bc133f404549601ec934ee66ecc735e42c
David Streeter <dstreet26@gmail.com>
2017-11-01 21:53:31 -0400

    heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -15,11 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :typeform_clone, TypeformCloneWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "aqueous-wildwood-54451.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :typeform_clone, TypeformClone.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 
 # ## SSL Support
 #
@@ -61,4 +69,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
